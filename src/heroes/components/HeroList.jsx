6e5688c171ffc26a6d2b57a238078a8e50594659
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-06-30 21:33:38 +0200

    fotos

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,14 @@
 import { getHeroesByPublisher } from '../helpers';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
-      {heroes.map((heroe) => (
-        <li key={heroe.id}>{heroe.superhero}</li>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
